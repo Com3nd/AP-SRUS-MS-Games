07c43ca1f6b0ea7b5d5b23eb31a8528057ffac57
Mustafa Salman <frostnote2@gmail.com>
2023-09-06 01:04:06 +0800

    Added bubble sort static method with testing

--- app/player.py
+++ app/player.py
@@ -1,6 +1,6 @@
 from argon2 import PasswordHasher
 from typing import Optional
-
+# import random
 
 class Player:
     """To be added"""
@@ -10,6 +10,7 @@ class Player:
         self._name: str = name
         self._hashed_password: Optional[str] = None
         self._ph = PasswordHasher()
+        self._score: int = 0
 
     @property
     def uid(self):
@@ -19,16 +20,54 @@ class Player:
     def name(self):
         return self._name
 
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value: int):
+        self._score = value if value >= 0 else 0
+
     def add_password(self, password: str):
         self._hashed_password = self._ph.hash(password)
 
     def verify_password(self, password) -> bool:
         return self._ph.verify(self._hashed_password, password)  # type: ignore
 
-    def __str__(self):
-        return f"Name: {self.name}"
+    @staticmethod
+    def bubble_sort(players: list['Player']):
+        for x in players:
+            for index, player in enumerate(players):
+                if index != len(players) - 1 and player < players[index + 1]:
+                    players[index] = players[index + 1]
+                    players[index + 1] = player
+
+        return players
+
+    def __gt__(self, other):
+        return self.score > other.score
+
+    def __lt__(self, other):
+        return self.score < other.score
+
+    def __eq__(self, other):
+        return self.score == other.score
+
+    def __repr__(self):
+        return f"Player({self.score})"
 
 
 if __name__ == "__main__":
-    mustafa = Player("AA", "Mustafa")
-    print(mustafa)
+    pass
+    # count = 0
+    # players = []
+    # while count < 29:
+    #     players.append(Player(f"{count}", f"Player {count}"))
+    #     players[count].score = count
+    #     count += 1
+    #
+    # random.shuffle(players)
+    #
+    # Player.bubble_sort(players)
+    # for player in players:
+    #     print(player.score)
--- test/player_test.py
+++ test/player_test.py
@@ -1,24 +1,46 @@
 from player import Player
 import unittest
-
+import random
 
 class TestPlayer(unittest.TestCase):
     def setUp(self) -> None:
-        self.player = Player("1", "Alice")
+        count = 0
+        self.players = []
+        while count < 10:
+            count += 1
+            self.players.append(Player(f"{count}", f"Player {count}"))
+            self.players[count - 1].score = count
 
     def test_name(self):
-        self.assertEqual(self.player.name, "Alice")
+        self.assertEqual(self.players[0].name, "Player 1")
 
     def test_uid(self):
-        self.assertEqual(self.player.uid, "1")
+        self.assertEqual(self.players[0].uid, "1")
 
     def test_add_password(self):
-        self.player.add_password("password")
-        self.assertIs(self.player._ph.verify(self.player._hashed_password, "password"), True)
+        self.players[0].add_password("password")
+        self.assertIs(self.players[0]._ph.verify(self.players[0]._hashed_password, "password"), True)
 
     def test_verify_password(self):
-        self.player.add_password("Password to verify")
-        self.assertIs(self.player.verify_password("Password to verify"), True)
+        self.players[0].add_password("Password to verify")
+        self.assertIs(self.players[0].verify_password("Password to verify"), True)
+
+    def test_comparison_operators(self):
+        self.players[0].score = 46
+        self.players[1].score = 10
+        self.players[2].score = 10
+
+        self.assertLess(self.players[1], self.players[0])
+        self.assertEqual(self.players[1], self.players[2])
+        self.assertGreater(self.players[0], self.players[2])
+
+    def test_bubble_sort(self):
+        random.shuffle(self.players)
+
+        unsort_list = self.players
+        Player.bubble_sort(self.players)
+
+        self.assertEqual(sorted(unsort_list, reverse=True), self.players)
 
 
 if __name__ == "__main__":
