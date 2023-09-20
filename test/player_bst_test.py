e685f1db718506f84fc416a9ed7304a0821b3a19
Mustafa Salman <frostnote2@gmail.com>
2023-09-20 14:22:22 +0800

    Added more tests for PlayerBST

--- test/player_bst_test.py
+++ test/player_bst_test.py
@@ -13,3 +13,21 @@ class TestPlayerBST(unittest.TestCase):
         self.bst.insert(Player("03", "player_3"))
 
         self.assertEqual(self.bst.root.player, Player("02", "player_2"))
+
+    def test_insert_right_leaf_node(self):
+        self.bst.insert(Player("02", "player_2"))
+        self.bst.insert(Player("03", "player_3"))
+        self.bst.insert(Player("04", "player_4"))
+        self.bst.insert(Player("05", "player_5"))
+
+        player_five = self.bst.root.right.right.right.player
+        self.assertEqual(player_five.name, Player("05", "player_5").name)
+
+    def test_insert_leaf_node(self):
+        self.bst.insert(Player("09", "player_9"))
+        self.bst.insert(Player("03", "player_3"))
+        self.bst.insert(Player("04", "player_4"))
+        self.bst.insert(Player("05", "player_5"))
+
+        player_five = self.bst.root.left.right.right.player
+        self.assertEqual(player_five.name, Player("05", "player_5").name)
